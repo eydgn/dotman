8774064f733bf4db1b9ca931a0e089d580ffca94
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-20 12:15:01 +0300

    feat(cli): add cli.h

---
+++ include/cli.h
@@ -0,0 +1,49 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "common.h"
+
+#define DOTMAN_VERSION "v0.1.0"
+typedef enum {
+  CMD_NONE = 0,
+  CMD_ADD,
+  CMD_DEL,
+  CMD_LIST,
+  CMD_EDIT,
+  CMD_SYNC,
+  CMD_INIT,
+  CMD_BACKUP,
+  CMD_HELP,
+  CMD_VERSION,
+  CMD_UNKNOWN,
+} cmd_type_t;
+
+typedef struct {
+  cmd_type_t   cmd;
+  scope_type_t scope;
+  str_vec_t*   fields;
+  str_vec_t*   array;
+} cli_cmd_type_t;
+
+cmd_type_t   extract_cmd(const char* cmd);
+scope_type_t extract_scope(const char* scp);
+int          extract_array_cli_cmd(cli_cmd_type_t* cli_cmd, char* data);
+int          parse_args(int argc, char** argv, cli_cmd_type_t* result);
+
+int cmd_add(cli_cmd_type_t* cmd);
+int cmd_del(cli_cmd_type_t* cmd);
+int cmd_list(cli_cmd_type_t* cmd);
+int cmd_edit(cli_cmd_type_t* cmd);
+int cmd_sync(cli_cmd_type_t* cmd);
+int cmd_init(cli_cmd_type_t* cmd);
+int cmd_backup(cli_cmd_type_t* cmd);
+int cmd_help(cli_cmd_type_t* cmd);
+int cmd_version(cli_cmd_type_t* cmd);
+int cmd_unknown(void);
+
+int execute_cmd(cli_cmd_type_t* cmd);
+
+#endif  // !CLI_H
