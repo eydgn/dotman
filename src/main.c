2557cb48618b764c75d200a7c462f1306253d35c
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-10-16 14:57:22 +0300

    test

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cfg.h"
+#include "core.h"
+
+int main(void) {
+  entry_t* entries;
+  entry_new(&entries);
+
+  read_cfg("test.cfg", &entries);
+  write_cfg(entries, "test.cfg");
+
+  printf("\nAfter sorting:\n");
+
+  for (size_t i = 0; i < entries->len; i++) {
+    printf("%s\n", *entries->data[i].entry->str);
+  }
+
+  for (size_t i = 0; i < entries->len; i++) {
+    svec_free(&entries->data[i].entry);
+  }
+
+  entry_free(&entries);
+  return 0;
+}
