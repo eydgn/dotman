3f5de0c0b57849987eaefc87fe445f1dc11a40a8
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-10-17 15:34:11 +0300

    feat(cli): add extract_action

---
+++ src/cli.c
@@ -0,0 +1,61 @@
+#include "cli.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+
+int extract_action(cmd_t* cmd, const char* action) {
+  if (!cmd || !action) {
+    LOG_ERROR("cmd or action is NULL");
+    return EXIT_FAILURE;
+  }
+
+  if (!(strcmp("add", action))) {
+    cmd->action = CMD_ADD;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("del", action))) {
+    cmd->action = CMD_DEL;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("list", action))) {
+    cmd->action = CMD_LIST;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("edit", action))) {
+    cmd->action = CMD_EDIT;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("sync", action))) {
+    cmd->action = CMD_SYNC;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("init", action))) {
+    cmd->action = CMD_INIT;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("backup", action))) {
+    cmd->action = CMD_BACKUP;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("help", action))) {
+    cmd->action = CMD_HELP;
+    return EXIT_SUCCESS;
+  }
+
+  if (!(strcmp("ver", action))) {
+    cmd->action = CMD_VER;
+    return EXIT_SUCCESS;
+  }
+
+  cmd->action = CMD_ERROR;
+  return EXIT_SUCCESS;
+}
